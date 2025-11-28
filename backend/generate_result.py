7f3ba83eb4b1f260d7164409db6d5d88ee9745d1
a123tony39 <a123tony39@gmail.com>
2025-11-28 22:49:57 +0800

    chore: restructure folders

--- backend/generate_result.py
+++ backend/generate_result.py
@@ -1,49 +1,53 @@
 from PIL import Image, ImageEnhance, ImageFilter, ImageDraw
 import easyocr
 import numpy as np
+import os
 
 reader = easyocr.Reader(['en', 'ch_tra'])  # 英文+中文
 
-template_name = "chisa_template.png"
-avatar_namae = "chisa.png"
-source_name = "input.png"
+img_path = "../img"
+template_name = os.path.join(img_path, "chisa_template.png")
+avatar_namae = os.path.join(img_path, "chisa.png")
+source_name = os.path.join(img_path, "input.png")
+output_path = os.path.join(img_path, "output.png")
 
-crop_areas = [
-    (0, 650, 380, 1050),
-    (380, 650, 380*2, 1050),
-    (380*2, 650, 380*3, 1050),
-    (380*3, 650, 380*4, 1050),
-    (380*4, 650, 380*5, 1050),
-]
+if __name__ == '__main__':
+    crop_areas = [
+        (0, 650, 380, 1050),
+        (380, 650, 380*2, 1050),
+        (380*2, 650, 380*3, 1050),
+        (380*3, 650, 380*4, 1050),
+        (380*4, 650, 380*5, 1050),
+    ]
 
-paste_positions = [
-    (1000, 40),
-    (1440, 40),
-    (560, 550),
-    (1000, 550),
-    (1440, 550)
-]
-# image open
-source = Image.open(source_name)
-template = Image.open(template_name)
+    paste_positions = [
+        (1000, 40),
+        (1440, 40),
+        (560, 550),
+        (1000, 550),
+        (1440, 550)
+    ]
+    # image open
+    source = Image.open(source_name)
+    template = Image.open(template_name)
 
-enhancer = ImageEnhance.Brightness(template)
-template = enhancer.enhance(0.5)
+    enhancer = ImageEnhance.Brightness(template)
+    template = enhancer.enhance(0.5)
 
-# crop
-for crop_area, paste_pos in zip(crop_areas, paste_positions):
-    cropped = source.crop(crop_area)
-    cropped_np = np.array(cropped)
-    results = reader.readtext(cropped_np)
-    
-    for bbox, text, prob in results:
-        print(f"文字: {text}, 信心指數: {prob:.2f}")
+    # crop
+    for crop_area, paste_pos in zip(crop_areas, paste_positions):
+        cropped = source.crop(crop_area)
+        cropped_np = np.array(cropped)
+        results = reader.readtext(cropped_np)
+        
+        for bbox, text, prob in results:
+            print(f"文字: {text}, 信心指數: {prob:.2f}")
 
-    cropped.thumbnail((400, 550))
-    template.paste(cropped, paste_pos)
+        cropped.thumbnail((400, 550))
+        template.paste(cropped, paste_pos)
 
-avatar = Image.open(avatar_namae)
-avatar.thumbnail((400, 550))
+    avatar = Image.open(avatar_namae)
+    avatar.thumbnail((400, 550))
 
-template.paste(avatar, (47, 37), avatar)
-template.save("output.png")
+    template.paste(avatar, (47, 37), avatar)
+    template.save(output_path)
