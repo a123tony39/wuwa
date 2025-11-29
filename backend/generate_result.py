6eb1131bd9f18744b831608a93da867cd08807f1
a123tony39 <a123tony39@gmail.com>
2025-11-29 21:31:09 +0800

    feat: 新增處理OCR結果以及計算聲骸評分功能

--- backend/generate_result.py
+++ backend/generate_result.py
@@ -1,15 +1,41 @@
-from PIL import Image, ImageEnhance, ImageFilter, ImageDraw
+from PIL import Image, ImageEnhance, ImageFilter, ImageDraw, ImageFont
 import easyocr
 import numpy as np
 import os
-
-
+from parsers.ocr_parser import parse_ocr_output
+from scoring.score import calculate_score, get_rank
 
 img_path = "../img"
 template_name = os.path.join(img_path, "chisa_template.png")
 avatar_namae = os.path.join(img_path, "chisa.png")
 source_name = os.path.join(img_path, "input.png")
+ss_score_name = os.path.join(img_path, "SS_score.png")
+s_score_name = os.path.join(img_path, "S_score.png")
+a_score_name = os.path.join(img_path, "A_score.png")
+b_score_name = os.path.join(img_path, "B_score.png")
 output_path = os.path.join(img_path, "output.png")
+font_path = "../ttf/Cubic_11.ttf"
+
+rank_images = {
+    "SS": ss_score_name,
+    "S": s_score_name,
+    "A": a_score_name,
+    "B": b_score_name
+}
+
+
+def add_black_border(img, width = 3):
+    draw = ImageDraw.Draw(img)
+    draw.rectangle((0, 0, img.width-1, img.height-1), outline="black", width=width)
+
+    return img
+
+def get_rank_pic(rank):
+    if rank in rank_images:
+        return Image.open(rank_images[rank])
+    else:
+        raise ValueError(f"{rank} is not valid ranking")
+
 
 if __name__ == '__main__':
     crop_areas = [
@@ -30,28 +56,55 @@ if __name__ == '__main__':
     # image open
     source = Image.open(source_name)
     template = Image.open(template_name)
+
+    # create ocr reader
     reader = easyocr.Reader(['en', 'ch_tra'])  # 英文+中文
     
+    # make template darker
     enhancer = ImageEnhance.Brightness(template)
     template = enhancer.enhance(0.5)
+    template_draw = ImageDraw.Draw(template)
 
-    # crop
+    # process echos
+    total_score = 0.0
     for crop_area, paste_pos in zip(crop_areas, paste_positions):
+        # crop
         cropped = source.crop(crop_area)
         cropped_np = np.array(cropped)
+        # read the text
         results = reader.readtext(cropped_np)
         
-        for bbox, text, prob in results:
-            print(f"文字: {text}, 信心指數: {prob:.2f}")
+        # process text
+        new_echo = parse_ocr_output(results)
+        
+        # calculate echo score
+        total_score += calculate_score(new_echo)
 
+        # paste echo to template
         cropped.thumbnail((400, 550))
+        cropped = add_black_border(cropped)
         template.paste(cropped, paste_pos)
 
+    # rank pic
+    rank = get_rank(total_score)
+    print(f"{rank}: {total_score}")
+    pic = get_rank_pic(rank)
+    pic.thumbnail((380, 400))
+    template.paste(pic, (560, 40))
+    # set text and font
+    text = f"聲骸評分: {total_score:.2f}"
+    font = ImageFont.truetype(font_path, 36)
+    # compute and align center
+    text_width = template_draw.textlength(text, font=font)
+    pic_center = 560 + pic.width//2
+    x = pic_center - text_width//2
+    template_draw.text((x, 40 + pic.height + 10), text, font = font, fill = (0, 0, 0))
+
+    # avatar
     avatar = Image.open(avatar_namae)
     avatar.thumbnail((400, 550))
-    draw = ImageDraw.Draw(avatar)
-    width = 3
-    draw.rectangle((0, 0, avatar.width-1, avatar.height-1), outline="black", width=width)
-
+    avatar = add_black_border(avatar)
     template.paste(avatar, (47, 37), avatar)
+
+    # save the result
     template.save(output_path)
