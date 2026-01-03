d22b6af97dfb42fd6735f0707f5bd28537454209
a123tony39 <a123tony39@gmail.com>
2026-01-04 04:34:51 +0800

    fix: 調整背景透明方法，避免Discord預覽背景過白問題

--- backend/render/background.py
+++ backend/render/background.py
@@ -5,12 +5,11 @@ def load_background(background_file, width, height):
     default_file = "../img/background/default.png"
     if not os.path.exists(background_file):
         background_file = default_file
-        background = Image.open(background_file).convert("RGBA")
-        background = background.resize((width, height), resample = Image.LANCZOS)
-    else:
-        background = Image.open(background_file).convert("RGBA")
-        background = background.resize((width, height), resample = Image.LANCZOS)
-        background.putalpha(80)
+    background = Image.open(background_file).convert("RGBA")
+    background = background.resize((width, height), resample = Image.LANCZOS)
+
+    overlay = Image.new("RGBA", background.size, (0, 0, 0, 150))
+    background = Image.alpha_composite(background, overlay)
     return background
 
 def combine_background_template(background, template):
